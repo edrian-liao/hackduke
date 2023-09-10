9e0dfb0ba007d0cbb1ed309d613f2fb21e66a122
jess <tjessica10@gmail.com>
2023-09-10 06:28:23 -0400

    Add Vue experiment

---
+++ front-end-test-vue/ui/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <b-navbar toggleable="lg" type="dark" variant="primary">
+      <b-navbar-brand href="#">DoseDocker</b-navbar-brand>      
+      <b-button @click="refresh">Refresh</b-button>
+    </b-navbar>
+    <!-- <b-form-group label="Patient Name:" label-for="patient-name-input">
+      <b-form-input id="patient-name-input" v-model="patientName" />
+    </b-form-group>
+    <b-button @click="search">Search</b-button> -->
+    
+    <b-table :items="data" />
+  </div>
+</template>
+
+<script setup lang="ts">
+import { onMounted, ref, Ref } from 'vue'
+
+const data: Ref<any[]> = ref([])
+
+async function refresh() {
+  const result = await fetch("/api/box_status")
+  data.value = await result.json()
+}
+onMounted(refresh)
+
+async function search() {
+  const result = await fetch("/api/box_status")
+  data.value = await result.json()
+}
+
+</script>
