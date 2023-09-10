74a4dd2006516fbfb525a79d093a1fb7bb7228d3
Edrian Liao <edrianpaul.liao@duke.edu>
2023-09-09 22:55:30 -0400

    queries

--- bundle.js
+++ bundle.js
@@ -1,3 +1,19 @@
+(function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
+module.exports=[
+    {
+        "name": "Edrian Liao",
+        "timestamps": ["2023-09-09 12:11:13","2023-09-10 16:30:32","2023-09-11 14:23:13"]
+    },
+    {
+        "name": "Annabelle Chu",
+        "timestamps": ["2023-09-09 12:08:07","2023-09-10 17:30:30","2023-09-11 15:01:37"]
+    },
+    {
+        "name": "Jessica Tiu",
+        "timestamps": ["2023-09-09 12:00:14","2023-09-10 16:28:21","2023-09-11 13:44:15"]
+    }
+]
+},{}],2:[function(require,module,exports){
 const jsonData = require('./sample1.json');
 
 // Get references to the input field and search button
@@ -39,3 +55,4 @@ function handleSearch() {
 
 // Add a click event listener to the search button
 searchButton.addEventListener('click', handleSearch);
+},{"./sample1.json":1}]},{},[2]);
