66d8e5c7064809d20b65bf330a625bb65166fa6c
Edrian Liao <edrianpaul.liao@duke.edu>
2023-09-10 07:37:22 -0400

    finished setting up design

--- front-end-test-vue/ui/src/App.vue
+++ front-end-test-vue/ui/src/App.vue
@@ -1,16 +1,116 @@
 <template>
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>DoseDocker</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Lato:wght@400&family=Roboto:wght@500&display=swap" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="style.css">
+  </head>
+</template>
+<template>
   <div>
     <b-navbar toggleable="lg" type="dark" variant="primary">
       <b-navbar-brand href="#">DoseDocker</b-navbar-brand>      
       <b-button @click="refresh">Refresh</b-button>
     </b-navbar>
-    <!-- <b-form-group label="Patient Name:" label-for="patient-name-input">
+    <b-form-group label="Patient Name:" label-for="patient-name-input">
       <b-form-input id="patient-name-input" v-model="patientName" />
     </b-form-group>
-    <b-button @click="search">Search</b-button> -->
+    <b-button @click="search">Search</b-button>
     
     <b-table :items="data" />
   </div>
+</template>
+<template>
+  <html lang="en">
+  <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>DoseDocker</title>
+      <link rel="preconnect" href="https://fonts.googleapis.com">
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+      <link href="https://fonts.googleapis.com/css2?family=Lato:wght@400&family=Roboto:wght@700&display=swap" rel="stylesheet">
+      <!-- <link rel="stylesheet" type="text/css" href="style.css"> -->
+  </head>
+  <body>
+      <div class = "search-container">
+          <label for="filter1">Patient Name: </label>
+          <input type="text" id="filter1" placeholder="e.g. Edrian Liao">
+          <label for="filter2">Date:  </label>
+          <input type="text" id="filter2" placeholder="Enter date here">
+          <button id="searchButton">Search</button>
+      </div>
+      
+
+      <!-- Box Status -->
+      <div class = "box">
+          <!-- Card per Day (7 card in total) -->
+          <div class = "card">
+              <h2>Sun</h2>
+              <p :items=data></p>
+          </div>
+          <div class = "card">
+              <h2>Mon</h2>
+          </div>
+          <div class = "card">
+              <h2>Tue</h2>
+          </div>
+          <div class = "card">
+              <h2>Wed</h2>
+          </div>
+          <div class = "card">
+              <h2>Thu</h2>
+          </div>
+          <div class = "card">
+              <h2>Fri</h2>
+          </div>
+          <div class = "card">
+              <h2>Sat</h2>
+          </div>
+      </div>
+      
+      <div class = "table-container">
+          <div class="table">
+              <h1>Patient Actions Log</h1>
+              <p>Insert description of this table</p>
+              <table id = "actions-log">
+                  <thead>
+                      <tr>
+                          <th>Patient</th>
+                          <th>Date</th>
+                          <th>Time</th>
+                      </tr>
+                  </thead>
+                  <tbody>
+                      <!-- Data will be populated here dynamically from MongoDB -->
+                  </tbody>
+              </table>
+          </div>
+
+
+          <div class="table">
+              <h1>Medicine Administration Log</h1>
+              <p>Insert description of this table</p>
+              <table id = "admin-log">
+                  <thead>
+                      <tr>
+                          <th>Patient</th>
+                          <th>Medicine</th>
+                          <th>Nurses-in-charge</th>
+                      </tr>
+                  </thead>
+                  <tbody>
+                      <!-- Data will be populated here dynamically from MongoDB -->
+                  </tbody>
+              </table>
+          </div>
+      </div>
+
+      <script type = "module" src="script.js"></script>
+  </body>
+  </html>
 </template>
 
 <script setup lang="ts">
@@ -30,3 +130,123 @@ async function search() {
 }
 
 </script>
+
+<style>
+  /* Global styles */
+  body {
+    align-items: center;
+    font-family: 'Lato', sans-serif;
+    max-width: 100%; /* Adjust this value to your desired maximum width */
+    margin: 30px auto 0;
+    padding: 30px;
+  }
+
+  h1, h2, caption{
+      font-family: 'Roboto', sans-serif;
+  }
+
+  /* Style for the search bar container */
+  .search-container {
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      max-width: 60%; /* Adjust the maximum width as needed */
+      margin: 0 auto;
+      padding: 20px;
+  }
+
+  /* Style for the input fields */
+  input[type="text"] {
+      flex: 1;
+      padding: 10px;
+      margin-left: 10px;
+      margin-right: 10px;
+      border: 1px solid #ccc;
+      border-radius: 5px;
+      font-family: 'Lato', sans-serif;
+      font-size: 16px;
+  }
+
+  /* Style for the search button */
+  button {
+      background-color: #7d817f;
+      color: #fff;
+      border: none;
+      border-radius: 20px;
+      margin-left: 10px;
+      padding: 10px 20px;
+      font-size: 16px;
+      cursor: pointer;
+  }
+
+  /* Hover effect for the search button */
+  button:hover {
+      background-color: #70E9AD;
+      color: #000;
+  }
+
+
+  .box {
+      max-width: 80%;
+      margin: 0 auto;
+      padding: 10px;
+      background-color: #f0f0f0;
+      border: 1px solid #ddd;
+      border-radius: 5px;
+      display: flex;
+      justify-content: space-between;
+      flex-wrap: wrap;
+  }
+
+  .card {
+      flex: 1;
+      width: calc(14.2857% - 20px); /* 100% / 7 cards - 20px margin */
+      /* padding-top: calc(14.2857% - 20px); Maintain a 1:1 aspect ratio */
+      margin: 10px;
+      background-color: #fff;
+      border: 1px solid #ddd;
+      border-radius: 5px;
+      box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      text-align: center;
+  }
+
+  .table-container {
+      max-width: 80%;
+      display: flex; /* Use flexbox for horizontal layout */
+      flex-wrap: wrap; /* Allow tables to wrap to the next line on smaller screens */
+      justify-content: space-between; /*Space tables evenly*/
+      margin: 20px auto;
+  }
+
+  .table {
+          background-color: #70E9AD;
+          width: auto;
+          padding: 20px;
+          border-radius: 5px;
+          box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
+          
+      }
+
+  table {
+      border-collapse: collapse;
+      width: 90%; /* Adjust the width as needed */
+      border: 1px solid #ddd;
+      margin: 5px;
+      align-items: center;
+  }
+
+  th, td {
+      border: 1px solid #ddd;
+      padding: 8px;
+      text-align: center;
+      background-color: #f2f2f2;
+  }
+  th {
+      background-color: #f2f2f2;
+  }
+
+  
+</style>
--- templates/script.js
+++ templates/script.js
@@ -63,46 +63,159 @@
 
 // main().catch(console.error);
 
-const { MongoClient } = require('mongodb');
+// var MongoClient = require('mongodb').MongoClient;
 
-const username = encodeURIComponent('annabelle');
-const password = encodeURIComponent('lwy9Ao3mpoDAVEeN');
-const uri = `mongodb+srv://${username}:${password}@hackduke.hasnibk.mongodb.net/?retryWrites=true&w=majority`;
+// const username = encodeURIComponent('annabelle');
+// const password = encodeURIComponent('lwy9Ao3mpoDAVEeN');
+// const uri = `mongodb+srv://${username}:${password}@hackduke.hasnibk.mongodb.net/?retryWrites=true&w=majority`;
 
+// // Get references to the input field and search button
+// const searchName = document.getElementById('filter1');
+// const searchDate = document.getElementById('filter2');
+// const searchButton = document.getElementById('searchButton');
 
+// async function connectToMongoDB() {
+//     const client = new MongoClient(uri);
 
-async function connectToMongoDB() {
-    const client = new MongoClient(uri);
+//     try {
+//         await client.connect();
+//         console.log('Connected to MongoDB!');
 
-    try {
-        await client.connect();
+//         const db = client.db('pillbox'); // Replace with your database name
+//         const collection = db.collection('box_status'); // Replace with your collection name
+
+//         const cursor = collection.find();
+//         // console.log(cursor);    
+
+//         await listDatabases(client);
+
+//         // Now, you can query the collection to access data
+//         const documents = await collection.find({}).toArray();
+//         console.log('Retrieved documents:', documents);
+
+//         const table = document.getElementById('actions-log');
+
+//         console.log(table.rows.length);
+//         while (table.rows.length > 1) {
+//             table.deleteRow(1);
+//         }
+//         // console.log(searchName);
+//         const index = documents.findIndex(obj => obj.name === searchName.value);
+//         console.log(index);
+//         const person = documents[index];
+//         // console.log(person);
+//         const length = person.timestamps.length;
+//         for (let i = 0; i < length; i++) {
+//             const newRow = table.insertRow();
+//             const name = person.name;
+//             const timestamp = person.timestamps[i];
+//             // console.log(timestamp);
+//             const [datePart, timePart] = timestamp.split(' ');
+            
+//             const cell0 = newRow.insertCell(0);
+//             const cell1 = newRow.insertCell(1);
+//             const cell2 = newRow.insertCell(2);
+        
+//             cell0.textContent = name;
+//             cell1.textContent = datePart;
+//             cell2.textContent = timePart;
+//         }
+//     } catch (e) {
+//         console.error('Error:', e);
+//     } finally {
+//         await client.close();
+//     }
+
+// }
+
+// async function listDatabases(client){
+//     databasesList = await client.db().admin().listDatabases();
+ 
+//     console.log("Databases:");
+//     databasesList.databases.forEach(db => console.log(` - ${db.name}`));
+// };
+
+// // Add a click event listener to the search button
+// searchButton.addEventListener('click', connectToMongoDB);
+
+var { MongoClient } = require('mongodb');
+
+var username = encodeURIComponent('annabelle');
+var password = encodeURIComponent('lwy9Ao3mpoDAVEeN');
+var uri = 'mongodb+srv://' + username + ':' + password + '@hackduke.hasnibk.mongodb.net/?retryWrites=true&w=majority';
+
+// Get references to the input field and search button
+// var searchName = document.getElementById('filter1');
+// var searchDate = document.getElementById('filter2');
+// var searchButton = document.getElementById('searchButton');
+
+function connectToMongoDB() {
+    var client = new MongoClient(uri);
+
+    client.connect().then(function () {
         console.log('Connected to MongoDB!');
 
-        const db = client.db('hackduke'); // Replace with your database name
-        const collection = db.collection('pillbox'); // Replace with your collection name
+        var db = client.db('pillbox'); // Replace with your database name
+        var collection = db.collection('box_status'); // Replace with your collection name
 
-        const cursor = collection.find();
-        // console.log(cursor);    
-        await listDatabases(client);
-    } catch (e) {
-        console.error('Error:', e);
-    } finally {
-        await client.close();
-    }
+        var cursor = collection.find();
 
-    
+        listDatabases(client);
 
+        // Now, you can query the collection to access data
+        collection.find({}).toArray().then(function (documents) {
+            console.log('Retrieved documents:', documents);
+
+            var table = document.getElementById('actions-log');
+
+            console.log(table.rows.length);
+            while (table.rows.length > 1) {
+                table.deleteRow(1);
+            }
+
+            var index = documents.findIndex(function (obj) {
+                return obj.name === searchName.value;
+            });
+
+            console.log(index);
+            var person = documents[index];
+
+            var length = person.timestamps.length;
+            for (var i = 0; i < length; i++) {
+                var newRow = table.insertRow();
+                var name = person.name;
+                var timestamp = person.timestamps[i];
+                var datePartTimePart = timestamp.split(' ');
+
+                var cell0 = newRow.insertCell(0);
+                var cell1 = newRow.insertCell(1);
+                var cell2 = newRow.insertCell(2);
+
+                cell0.textContent = name;
+                cell1.textContent = datePartTimePart[0];
+                cell2.textContent = datePartTimePart[1];
+            }
+        }).catch(function (e) {
+            console.error('Error:', e);
+        }).finally(function () {
+            client.close();
+        });
+    });
 }
 
-async function listDatabases(client){
-    databasesList = await client.db().admin().listDatabases();
- 
-    console.log("Databases:");
-    databasesList.databases.forEach(db => console.log(` - ${db.name}`));
-};
-
-
+function listDatabases(client) {
+    client.db().admin().listDatabases().then(function (databasesList) {
+        console.log("Databases:");
+        databasesList.databases.forEach(function (db) {
+            console.log(' - ' + db.name);
+        });
+    });
+}
 
 connectToMongoDB();
+
+// Add a click event listener to the search button
+// searchButton.addEventListener('click', connectToMongoDB);
+
 
 
